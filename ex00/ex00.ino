f48864fd782cc24b1ebfc2e086e662e3f8e78969
Polina Simonenko <psimonen@c2s15d84.42wolfsburg.de>
2023-07-27 16:48:44 +0200

    improved ex00

--- ex00/ex00.ino
+++ ex00/ex00.ino
@@ -1,26 +1,67 @@
-const int buttonPin = 7;
-const int greenLedPin = 12;
-const int redLedPin = 13;
-int buttonState = 0;
+const int 		buttonPin = 7;
+const int 		greenLedPin = 12;
+const int 		redLedPin = 13;
+
+int				buttonCounter = 0;
+int 			buttonState = LOW;
+int				prevButtonState = LOW;
+
+int				redLedState = LOW;
+int				greenLedState = LOW;
+
+unsigned long	prevTime = 0;
 
 void setup() {
   pinMode(greenLedPin, OUTPUT);
   pinMode(redLedPin, OUTPUT);
   pinMode(buttonPin, INPUT);
-  digitalWrite(greenLedPin, HIGH);
+}
+
+int	checkTime(int delay)
+{
+  	unsigned long	currTime;
+  
+  	currTime = millis();
+  	if (currTime - prevTime >= delay)
+    {
+      prevTime = currTime;
+      return (1);
+    }
+  	return (0);
 }
 
 void loop() {
   buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) {
-    digitalWrite(greenLedPin, LOW);
-    while(1)
+  if (buttonState == HIGH && buttonState != prevButtonState)
+    buttonCounter += 1;
+  if (buttonCounter % 2 == 1) {
+    if (greenLedState == HIGH)
     {
-    	digitalWrite(redLedPin, HIGH);
-      	delay(1000);
-      	digitalWrite(redLedPin, LOW);
-        delay(1000);
+    	greenLedState = LOW;
+     	digitalWrite(greenLedPin, LOW);
+    }
+    if (checkTime(1000))
+    {
+      	if (redLedState == LOW)
+          redLedState = HIGH;
+        else
+          redLedState = LOW;
+	    digitalWrite(redLedPin, redLedState);
     }
   }
+  else
+  {
+    if (redLedState == HIGH)
+    {
+     	redLedState = LOW;
+    	digitalWrite(redLedPin, LOW);
+    }
+    if (greenLedState == LOW)
+    {
+    	greenLedState = HIGH;
+      	digitalWrite(greenLedPin, HIGH);
+    }
+  }
+  prevButtonState = buttonState;
 }
 
