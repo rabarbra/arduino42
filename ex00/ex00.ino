db06b6202286f438f883d42ab72b5ce62f51bdae
Polina Simonenko <rabarbrablad@gmail.com>
2023-07-27 20:05:10 +0200

    .

--- ex00/ex00.ino
+++ ex00/ex00.ino
@@ -1,20 +1,21 @@
-const int 		buttonPin = 7;
-const int 		greenLedPin = 12;
-const int 		redLedPin = 13;
+const int       buttonPin = 7;
+const int       greenLedPin = 12;
+const int       redLedPin = 13;
 
-int				buttonCounter = 0;
-int 			buttonState = LOW;
-int				prevButtonState = LOW;
+int             buttonCounter = 0;
+int             buttonState = LOW;
+int             prevButtonState = LOW;
 
-int				redLedState = LOW;
-int				greenLedState = LOW;
+int             redLedState = LOW;
+int             greenLedState = LOW;
 
-unsigned long	prevTime = 0;
+unsigned long   prevTime = 0;
 
-void setup() {
-  pinMode(greenLedPin, OUTPUT);
-  pinMode(redLedPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
+void setup()
+{
+    pinMode(greenLedPin, OUTPUT);
+    pinMode(redLedPin, OUTPUT);
+    pinMode(buttonPin, INPUT);
 }
 
 int	checkTime(int delay)
@@ -24,44 +25,45 @@ int	checkTime(int delay)
   	currTime = millis();
   	if (currTime - prevTime >= delay)
     {
-      prevTime = currTime;
-      return (1);
+        prevTime = currTime;
+        return (1);
     }
   	return (0);
 }
 
-void loop() {
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH && buttonState != prevButtonState)
-    buttonCounter += 1;
-  if (buttonCounter % 2 == 1) {
-    if (greenLedState == HIGH)
+void loop()
+{
+    buttonState = digitalRead(buttonPin);
+    if (buttonState == HIGH && buttonState != prevButtonState)
+        buttonCounter += 1;
+    if (buttonCounter % 2 == 1)
     {
-    	greenLedState = LOW;
-     	digitalWrite(greenLedPin, LOW);
+        if (greenLedState == HIGH)
+        {
+        	  greenLedState = LOW;
+         	  digitalWrite(greenLedPin, LOW);
+        }
+        if (checkTime(1000))
+        {
+          	if (redLedState == LOW)
+                redLedState = HIGH;
+            else
+                redLedState = LOW;
+          digitalWrite(redLedPin, redLedState);
+        }
     }
-    if (checkTime(1000))
+    else
     {
-      	if (redLedState == LOW)
-          redLedState = HIGH;
-        else
-          redLedState = LOW;
-	    digitalWrite(redLedPin, redLedState);
+        if (redLedState == HIGH)
+        {
+         	  redLedState = LOW;
+        	  digitalWrite(redLedPin, LOW);
+        }
+        if (greenLedState == LOW)
+        {
+        	  greenLedState = HIGH;
+          	digitalWrite(greenLedPin, HIGH);
+        }
     }
-  }
-  else
-  {
-    if (redLedState == HIGH)
-    {
-     	redLedState = LOW;
-    	digitalWrite(redLedPin, LOW);
-    }
-    if (greenLedState == LOW)
-    {
-    	greenLedState = HIGH;
-      	digitalWrite(greenLedPin, HIGH);
-    }
-  }
-  prevButtonState = buttonState;
+    prevButtonState = buttonState;
 }
-
